df0dbc6dde886355a2febe7dd8f162970dd92dc5
topeakeredolu <tope.akeredolu@gmail.com>
2024-06-10 19:50:26 +0100

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num=8;
+        if (num>10){
+            System.out.println("num is greater than 10");
+        }else 
+        System.out.println("Hello world!");
+    }
+}
